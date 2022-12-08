86ecc15a4b574d6efea05669be994351dd23ddb3
Jens Holmberg <jens.holmberg@protonmail.com>
2022-12-08 20:46:07 +0200

    Add bench for day 4

--- src/main/kotlin/io/github/nillkki/aoc/Day04.kt
+++ src/main/kotlin/io/github/nillkki/aoc/Day04.kt
@@ -1,50 +1,81 @@
 package io.github.nillkki.aoc
 
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Measurement
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.Warmup
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 1)
+@Measurement(iterations = 3)
+class Day04 {
+    companion object {
+        fun toRange(section: String): IntRange {
+            val ids = section.split('-')
+            return IntRange(ids.first().toInt(), ids.last().toInt())
+        }
+
+        fun part1(input: List<String>): Int {
+            var sum = 0
+
+            for (i in input) {
+                val pair = i.split(',')
+                val firstElf = toRange(pair.first())
+                val secondElf = toRange(pair.last())
+
+                if (firstElf.all { secondElf.contains(it) }
+                    || secondElf.all { firstElf.contains(it) }) {
+                    sum += 1
+                }
+            }
+
+            return sum
+        }
+
+        fun part2(input: List<String>): Int {
+            var sum = 0
+
+            for (i in input) {
+                val pair = i.split(',')
+                val firstElf = toRange(pair.first())
+                val secondElf = toRange(pair.last())
+
+                if (firstElf.any { secondElf.contains(it) } || secondElf.any { firstElf.contains(it) }) {
+                    sum += 1
+                }
+            }
+
+            return sum
+        }
+    }
+
+    private var input = emptyList<String>()
+
+    @Setup
+    fun setUp() {
+        input = readInput("Day04")
+    }
+
+    @Benchmark
+    public fun part1Bench() {
+        part1(input)
+    }
+
+    @Benchmark
+    public fun part2Bench() {
+        part2(input)
+    }
+}
+
 fun main() {
-    fun toRange(section: String): IntRange {
-        val ids = section.split('-')
-        return IntRange(ids.first().toInt(), ids.last().toInt())
-    }
-
-    fun part1(input: List<String>): Int {
-        var sum = 0
-
-        for (i in input) {
-            val pair = i.split(',')
-            val firstElf = toRange(pair.first())
-            val secondElf = toRange(pair.last())
-
-            if (firstElf.all { secondElf.contains(it) }
-                || secondElf.all { firstElf.contains(it) }) {
-                sum += 1
-            }
-        }
-
-        return sum
-    }
-
-    fun part2(input: List<String>): Int {
-        var sum = 0
-
-        for (i in input) {
-            val pair = i.split(',')
-            val firstElf = toRange(pair.first())
-            val secondElf = toRange(pair.last())
-
-            if (firstElf.any { secondElf.contains(it) } || secondElf.any { firstElf.contains(it) }) {
-                sum += 1
-            }
-        }
-
-        return sum
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 2)
-    check(part2(testInput) == 4)
+    check(Day04.part1(testInput) == 2)
+    check(Day04.part2(testInput) == 4)
 
     val input = readInput("Day04")
-    println(part1(input))
-    println(part2(input))
+    println(Day04.part1(input))
+    println(Day04.part2(input))
 }
