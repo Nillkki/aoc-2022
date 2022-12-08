fe9a83d956987c4dfec6add7bd253301c6269a24
Jens Holmberg <jens.holmberg@protonmail.com>
2022-12-08 21:30:40 +0200

    Add bench for day 5

--- src/main/kotlin/io/github/nillkki/aoc/Day05.kt
+++ src/main/kotlin/io/github/nillkki/aoc/Day05.kt
@@ -1,90 +1,120 @@
 package io.github.nillkki.aoc
 
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Measurement
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.Warmup
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 1)
+@Measurement(iterations = 3)
+class Day05 {
+    companion object {
+        fun parseParts(input: String): Pair<List<String>, List<String>> {
+            val parts = input.split("\n\n")
+            val cargo = parts.first().split("\n")
+            val moves = parts.last().split("\n")
+            return cargo to moves
+        }
+
+        fun parseStacks(cargo: List<String>): Array<ArrayDeque<Char>> {
+            val lines = cargo.reversed().iterator()
+
+            // Build stacks first
+            val firstLine = lines.next()
+            val numberOfStacks = firstLine.split("   ").size
+            val stacks = Array(numberOfStacks) {
+                ArrayDeque<Char>()
+            }
+
+            lines.forEachRemaining {
+                val columns = it.chunked(4)
+
+                columns.forEachIndexed { index, element ->
+                    if (element.isNotBlank()) {
+                        // Add container to current stack
+                        stacks[index].add(element[1])
+                    }
+                }
+            }
+
+            return stacks
+        }
+
+        fun parseMove(move: String): Triple<Int, Int, Int> {
+            val moveParts = move.split(" ")
+            val count = moveParts[1].toInt()
+            val from = moveParts[3].toInt()
+            val to = moveParts[5].toInt()
+            return Triple(count, from, to)
+        }
+
+        fun part1(input: String): String {
+            val (cargo, moves) = parseParts(input)
+            val stacks = parseStacks(cargo)
+
+            moves.forEach {
+                if(it.isNotBlank()) {
+                    val (count, from, to) = parseMove(it)
+                    repeat(count) {
+                        val element = stacks[from-1].removeLast()
+                        stacks[to-1].add(element)
+                    }
+                }
+            }
+
+            return stacks.map { it.last() }.joinToString(separator = "")
+        }
+
+        fun part2(input: String): String {
+            val (cargo, moves) = parseParts(input)
+            val stacks = parseStacks(cargo)
+
+            moves.forEach {
+                if(it.isNotBlank()) {
+                    val (count, from, to) = parseMove(it)
+
+                    // Crane should keep order of moved items
+                    val crane = ArrayDeque<Char>()
+                    repeat(count) {
+                        val element = stacks[from-1].removeLast()
+                        crane.addFirst(element)
+                    }
+                    stacks[to-1].addAll(crane)
+                }
+            }
+
+            return stacks.map { it.last() }.joinToString(separator = "")
+        }
+    }
+
+    private var input = ""
+
+    @Setup
+    fun setUp() {
+        input = readInputAsText("Day05")
+    }
+
+    @Benchmark
+    public fun part1Bench() {
+        part1(input)
+    }
+
+    @Benchmark
+    public fun part2Bench() {
+        part2(input)
+    }
+}
+
 fun main() {
-
-    fun parseParts(input: String): Pair<List<String>, List<String>> {
-        val parts = input.split("\n\n")
-        val cargo = parts.first().split("\n")
-        val moves = parts.last().split("\n")
-        return cargo to moves
-    }
-
-    fun parseStacks(cargo: List<String>): Array<ArrayDeque<Char>> {
-        val lines = cargo.reversed().iterator()
-
-        // Build stacks first
-        val firstLine = lines.next()
-        val numberOfStacks = firstLine.split("   ").size
-        val stacks = Array(numberOfStacks) {
-            ArrayDeque<Char>()
-        }
-
-        lines.forEachRemaining {
-            val columns = it.chunked(4)
-
-            columns.forEachIndexed { index, element ->
-                if (element.isNotBlank()) {
-                    // Add container to current stack
-                    stacks[index].add(element[1])
-                }
-            }
-        }
-
-        return stacks
-    }
-
-    fun parseMove(move: String): Triple<Int, Int, Int> {
-        val moveParts = move.split(" ")
-        val count = moveParts[1].toInt()
-        val from = moveParts[3].toInt()
-        val to = moveParts[5].toInt()
-        return Triple(count, from, to)
-    }
-
-    fun part1(input: String): String {
-        val (cargo, moves) = parseParts(input)
-        val stacks = parseStacks(cargo)
-
-        moves.forEach {
-            if(it.isNotBlank()) {
-                val (count, from, to) = parseMove(it)
-                repeat(count) {
-                    val element = stacks[from-1].removeLast()
-                    stacks[to-1].add(element)
-                }
-            }
-        }
-
-        return stacks.map { it.last() }.joinToString(separator = "")
-    }
-
-    fun part2(input: String): String {
-        val (cargo, moves) = parseParts(input)
-        val stacks = parseStacks(cargo)
-
-        moves.forEach {
-            if(it.isNotBlank()) {
-                val (count, from, to) = parseMove(it)
-
-                // Crane should keep order of moved items
-                val crane = ArrayDeque<Char>()
-                repeat(count) {
-                    val element = stacks[from-1].removeLast()
-                    crane.addFirst(element)
-                }
-                stacks[to-1].addAll(crane)
-            }
-        }
-
-        return stacks.map { it.last() }.joinToString(separator = "")
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInputAsText("Day05_test")
-    check(part1(testInput) == "CMZ")
-    check(part2(testInput) == "MCD")
+    check(Day05.part1(testInput) == "CMZ")
+    check(Day05.part2(testInput) == "MCD")
 
     val input = readInputAsText("Day05")
-    println(part1(input))
-    println(part2(input))
+    println(Day05.part1(input))
+    println(Day05.part2(input))
 }
